445d13dfced9a127d5514984365db195bff7714d
chao <xiaochao200@126.com>
2024-02-21 16:51:46 +0800

    commit ch3.3.2

---
+++ Ch3/Ch3.3/ex3.14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> res;
+    int i;
+    while (cin >> i)
+    {
+        res.push_back(i);
+    }
+    return 0;
+}
---
+++ Ch3/Ch3.3/ex3.15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<string> res;
+    string i;
+    while (cin >> i)
+    {
+        res.push_back(i);
+    }
+    return 0;
+}
